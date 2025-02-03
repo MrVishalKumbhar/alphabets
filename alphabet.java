a3ea3a14e8ec9614b652899260ed83c430ea790d
Mr Vishal Kumbhar <vkvishalkumbhar99@gmail.com>
2025-02-03 22:32:59 +0530

    #Welcome Method

--- alphabet.java
+++ alphabet.java
@@ -1,6 +1,27 @@
-// #Main Class
+import java.util.Scanner;
+// #Main_Class
 public class alphabet {
+    Scanner s = new Scanner(System.in);
+
+    // #Welcome_Method
+    public void welcome() {
+        System.out.println("========== Welcome to the Alphabet Program ==========\n Please Enter Any Alphabets You Want : ");
+        String AlpInput = s.next();
+
+        // #Choose_alphabet_Method
+        switch (AlpInput) {
+            case "A":
+
+                break;
+        
+            default:
+                System.out.println("Invalid Input");
+                break;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        alphabet alp = new alphabet();
+        alp.welcome();
     }
 }
